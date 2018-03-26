414497910be8ea389446aae51d83d7954fb9dfe4
Artyom <artyom-ivanov-spb@yandex.ru>
2018-03-27 00:00:42 +0300

    fixed date x1

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const KB = {
 /*==================ЗАДАНИЕ ДАТЫ=====================*/
 
 moment.locale('ru')
-const week = moment().month().date().format("DD MMM")+" – "+moment().month().date(7).format("DD MMM");
+const week = moment().month().day(1).format("DD MMM")+" – "+moment().month().day(7).format("DD MMM");
 const day = moment().format("DD MMM")+ ", " +moment().format('dddd');
 const weekNumber = moment().isoWeek();
 const mondayThisWeek = "Вы смотрите расписание на "+moment().weekday(0).format("DD MMM"+", "+"dddd");
