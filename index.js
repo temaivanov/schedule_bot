703cacc6ec1697e1846ee118be5f13320d0a9495
Artyom <artyom-ivanov-spb@yandex.ru>
2018-04-11 23:20:53 +0300

    test

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ let weekNumber = moment().isoWeek();
 //let tuesdayThisWeek = "Вы смотрите расписание на "+moment().weekday(1).format("DD MMM"+", "+"dddd");
 //let wednesdayThisWeek = "Вы смотрите расписание на "+moment().weekday(2).format("DD MMM"+", "+"dddd");
 //let thursdayThisWeek = "Вы смотрите расписание на "+moment().weekday(3).format("DD MMM"+", "+"dddd");
-let time = moment();
+if (true) let time = moment();
 //let time = m.format('mm:ss');
 /*====================================================*/
 
