1cf6e47a467a1350f931c9b07b8179b376fad1f8
Artyom <artyom-ivanov-spb@yandex.ru>
2018-04-11 21:19:11 +0300

    test

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const KB = {
 moment.locale('ru')
 //const week = moment().day("Monday")+" – "+moment().day("Sunday");
 //let day = moment().format("DD MMM, dddd");
+let time = moment().format('HH:mm');
 let weekNumber = moment().isoWeek();
 //let mondayThisWeek = "Вы смотрите расписание на "+moment().weekday(0).format("DD MMM"+", "+"dddd");
 //let tuesdayThisWeek = "Вы смотрите расписание на "+moment().weekday(1).format("DD MMM"+", "+"dddd");
@@ -32,7 +33,7 @@ let weekNumber = moment().isoWeek();
 /*====================================================*/
 
 bot.onText(/\/start/, (msg) => {
-  const text = `Привет, ${msg.from.first_name}\n\nТекущая неделя ${moment().isoWeek()}-ая в 2018 году, ${isEven(weekNumber)}\nСегодня: ${moment().format("DD MMM, dddd")}\nВремя: ${moment().format('HH:mm')}\nДанный бот покажет расписание занятий на текущей неделе.\n\nРасписание на какой день вам нужно?`
+  const text = `Привет, ${msg.from.first_name}\n\nТекущая неделя ${moment().isoWeek()}-ая в 2018 году, ${isEven(weekNumber)}\nСегодня: ${moment().format("DD MMM, dddd")}\nВремя: ${time}\nДанный бот покажет расписание занятий на текущей неделе.\n\nРасписание на какой день вам нужно?`
   bot.sendMessage(msg.chat.id, text, {
     reply_markup: {
       keyboard: [
@@ -81,3 +82,5 @@ function isEven (){
     return ('нечётная неделя')
   }
 }
+
+console.log(weekNumber);
